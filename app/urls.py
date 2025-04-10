57085ecf30a96754e8da17b69b8513576d223751
9527 <161349777+Alexaenr@users.noreply.github.com>
2025-04-10 23:41:57 +0800

    个人信息

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
-from django.template.context_processors import static
 from django.urls import path
 from app import views
-from walmart import settings
+
+
+
+
 
 urlpatterns = [
     path("login/", views.login,name = "login"),
@@ -9,5 +11,4 @@ urlpatterns = [
     path('logOut/', views.logOut,name = 'logOut'),
     path('chargeSelfInfo/', views.chargeSelfInfo, name='chargeSelfInfo'),
     path("home/",views.home,name = "home"),
-
 ]
